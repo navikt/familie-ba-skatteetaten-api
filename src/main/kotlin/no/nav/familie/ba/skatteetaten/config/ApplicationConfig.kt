43afc753eccdda91b062cc2c67c760b5c85405f9
Stig Strøm <stig.helge.strom@nav.no>
2021-08-27 11:47:12 +0200

    Oppsett app (#1)
    Enkelt skall for en ny app

---
+++ src/main/kotlin/no/nav/familie/ba/skatteetaten/config/ApplicationConfig.kt
@@ -0,0 +1,33 @@
+package no.nav.familie.ba.skatteetaten.config
+
+import no.nav.familie.log.filter.LogFilter
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.slf4j.LoggerFactory
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootConfiguration
+@ConfigurationPropertiesScan
+@ComponentScan(ApplicationConfig.pakkenavn)
+@EnableScheduling
+@EnableJwtTokenValidation(ignore = ["org.springframework","springfox.documentation.swagger"])
+class ApplicationConfig {
+
+    @Bean
+    fun logFilter(): FilterRegistrationBean<LogFilter> {
+        log.info("Registering LogFilter filter")
+        val filterRegistration: FilterRegistrationBean<LogFilter> = FilterRegistrationBean()
+        filterRegistration.filter = LogFilter()
+        filterRegistration.order = 1
+        return filterRegistration
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(this::class.java)
+        const val pakkenavn = "no.nav.familie.ba.skatteetaten"
+    }
+}
