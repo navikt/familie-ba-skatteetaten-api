d4f54a01e711621e2f3b13df5abfad6d3e309c65
Stig Strøm <stig.helge.strom@nav.no>
2021-09-20 11:54:08 +0200

    Oppdatert til å bruke kontrakt (#9)

--- src/main/kotlin/no/nav/familie/ba/skatteetaten/service/SkatteetatenService.kt
+++ src/main/kotlin/no/nav/familie/ba/skatteetaten/service/SkatteetatenService.kt
@@ -3,21 +3,21 @@ package no.nav.familie.ba.skatteetaten.service
 
 
 import no.nav.familie.ba.skatteetaten.integrasjoner.BasakClient
-import no.nav.familie.ba.skatteetaten.model.PerioderRequest
-import no.nav.familie.ba.skatteetaten.model.PerioderResponse
-import no.nav.familie.ba.skatteetaten.model.PersonerResponse
+import no.nav.familie.eksterne.kontrakter.skatteetaten.SkatteetatenPerioderRequest
+import no.nav.familie.eksterne.kontrakter.skatteetaten.SkatteetatenPerioderResponse
+import no.nav.familie.eksterne.kontrakter.skatteetaten.SkatteetatenPersonerResponse
 import org.springframework.stereotype.Service
 
 @Service
 class SkatteetatenService(val basakClient: BasakClient) {
 
     fun hentPerioderMedUtvidetBarnetrygd(
-        perioderRequest: PerioderRequest
-    ): PerioderResponse {
+        perioderRequest: SkatteetatenPerioderRequest
+    ): SkatteetatenPerioderResponse {
         return basakClient.hentPerioder(perioderRequest)
     }
 
-    fun finnPersonerMedUtvidetBarnetrygd(aar: String): PersonerResponse {
+    fun finnPersonerMedUtvidetBarnetrygd(aar: String): SkatteetatenPersonerResponse {
         return basakClient.hentPersoner(aar)
     }
 }
