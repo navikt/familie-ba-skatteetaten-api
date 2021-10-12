0c4fa17044cfe67172201ca727509b8d19d93b6c
Stig Strøm <stig.helge.strom@nav.no>
2021-10-12 12:40:05 +0200

    Refaktorering clientkall til egne klasser for å få bedre metrikker (#31)

--- src/main/kotlin/no/nav/familie/ba/skatteetaten/service/SkatteetatenService.kt
+++ src/main/kotlin/no/nav/familie/ba/skatteetaten/service/SkatteetatenService.kt
@@ -1,23 +1,23 @@
 package no.nav.familie.ba.skatteetaten.service
 
 
-
-import no.nav.familie.ba.skatteetaten.integrasjoner.BasakClient
+import no.nav.familie.ba.skatteetaten.integrasjoner.BasakPerioderClient
+import no.nav.familie.ba.skatteetaten.integrasjoner.BasakPersonerClient
 import no.nav.familie.eksterne.kontrakter.skatteetaten.SkatteetatenPerioderRequest
 import no.nav.familie.eksterne.kontrakter.skatteetaten.SkatteetatenPerioderResponse
 import no.nav.familie.eksterne.kontrakter.skatteetaten.SkatteetatenPersonerResponse
 import org.springframework.stereotype.Service
 
 @Service
-class SkatteetatenService(val basakClient: BasakClient) {
+class SkatteetatenService(val basakPersonerClient: BasakPersonerClient, val basakPerioderClient: BasakPerioderClient) {
 
     fun hentPerioderMedUtvidetBarnetrygd(
         perioderRequest: SkatteetatenPerioderRequest
     ): SkatteetatenPerioderResponse {
-        return basakClient.hentPerioder(perioderRequest)
+        return basakPerioderClient.hentPerioder(perioderRequest)
     }
 
     fun finnPersonerMedUtvidetBarnetrygd(aar: String): SkatteetatenPersonerResponse {
-        return basakClient.hentPersoner(aar)
+        return basakPersonerClient.hentPersoner(aar)
     }
 }
