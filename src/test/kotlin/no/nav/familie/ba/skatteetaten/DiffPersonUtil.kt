df6304f43b2e2bf98c81b8f009a27f7aadd4d3f3
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-04-04 14:31:45 +0200

    Verktøy for å gjøre diff

---
+++ src/test/kotlin/no/nav/familie/ba/skatteetaten/DiffPersonUtil.kt
@@ -0,0 +1,39 @@
+package no.nav.familie.ba.skatteetaten
+
+import no.nav.familie.eksterne.kontrakter.skatteetaten.SkatteetatenPersonerResponse
+import no.nav.familie.kontrakter.felles.objectMapper
+import org.springframework.util.StopWatch
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import kotlin.io.path.Path
+
+
+/**
+ * Sammenligner 2 uttrekk av personer-kallet.
+ */
+fun main() {
+    val personer = objectMapper.readValue(
+        Path("./person-før.json").toFile(),
+        SkatteetatenPersonerResponse::class.java
+    ).brukere.map { it.ident }.sorted()
+    val personerEtter = objectMapper.readValue(
+        Path("./person-etter.json").toFile(),
+        SkatteetatenPersonerResponse::class.java
+    ).brukere.map { it.ident }.sorted()
+    val stopWatch = StopWatch()
+    stopWatch.start()
+    val diffFørEtter = personer.minus(personerEtter.toHashSet())
+    val diffEtterFør = personerEtter.minus(personer.toHashSet())
+    stopWatch.stop()
+    println("Finnes i person-før, men ikke i etter: $diffFørEtter")
+    println("Finnes i person-etter, men ikke i før: $diffEtterFør")
+    println("Tid: ${stopWatch.totalTimeSeconds}s")
+
+}
+
+private fun lesFil(filnavn: String): String {
+    return Files.readString(
+        Path(filnavn),
+        StandardCharsets.UTF_8
+    )
+}
